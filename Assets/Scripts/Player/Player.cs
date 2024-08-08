5bc8caa21569973e5fbdb40da9ffa4ebc92b7fa6
DanielDiazDev <danieldiazdev00@gmail.com>
2024-08-08 17:54:25 -0300

    A jump and movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speedMovement;
+    //Jump
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _groundLayerMask;
+    [SerializeField] private Transform _groundController;
+    [SerializeField] private Vector3 _size;
+    [SerializeField] private bool _isGrounded;
+    private bool _isJump;
     private Rigidbody2D _rigidbody;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJump = true;
+        }
+    }
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_speedMovement, _rigidbody.velocity.y);
+        _isGrounded = Physics2D.OverlapBox(_groundController.position, _size, 0, _groundLayerMask);
+        Jump();
+        _isJump = false;
+    }
+
+    private void Jump()
+    {
+        if(_isGrounded && _isJump)
+        {
+            _isGrounded = false;
+            _rigidbody.AddForce(new Vector2(0, _jumpForce));
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(_groundController.position, _size);
     }
 }
